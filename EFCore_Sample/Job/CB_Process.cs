113e37ce5f085f0f1471ef55261c37692ab5e67b
Sergii_Dell_laptop <grinevich.sergey.official@gmail.com>
2023-09-08 17:28:53 +0300

    remove not used code

--- EFCore_Sample/Job/CB_Process.cs
+++ EFCore_Sample/Job/CB_Process.cs
@@ -1,9 +1,7 @@
 ﻿using EFCore_Sample.EntityModels.Phrase;
-using EFCore_Sample.Helpers;
 using EFCore_Sample.Models;
 using Quartz;
 using Telegram.Bot;
-using Telegram.Bot.Polling;
 
 namespace EFCore_Sample.Job
 {
@@ -25,57 +23,17 @@ namespace EFCore_Sample.Job
                 int id;
                 PhraseEntity phraseEntity = new PhraseEntity(_context);
                 var phrases = phraseEntity.GetData();
-          //      foreach(var phrase in phrases)
-          //      {
-          //          Console.WriteLine($"id: {phrase.id}, text: {phrase.text}");
-          //      }
                 int maxId = phrases.Select(e => e.id).Max(); 
 
                 Random random = new Random();
                 id = random.Next(maxId);
 
-                var t = await _bot.SendTextMessageAsync(ConfigurationApp.BotId, phrases[id].text); //  "@channelname or chat_id", "text message");
-
-        //        await Task.Delay(10000);
-
-        //        var cts = new CancellationTokenSource();
-        //        var cancellationToken = cts.Token;
-        //        var receiverOptions = new ReceiverOptions
-        //        {
-        //            AllowedUpdates = { },
-        //        };
-                /*
-                _bot.StartReceiving(
-                    TGBotHelper.HandleUpdateAsync,
-                    TGBotHelper.HandleErrorAsync,
-                    receiverOptions,
-                    cancellationToken
-                );
-                */
+                var t = await _bot.SendTextMessageAsync(ConfigurationApp.BotId, phrases[id].text);
             }
             catch (Exception ex)
             {
                 throw new Exception(ex.Message, ex);
             }
         }
-        /*
-         * to get Employees
-        public async Task Execute(IJobExecutionContext context)
-        {
-            try
-            {
-                EmployeeEntity employeeEntity = new EmployeeEntity(_context);
-                var employees = employeeEntity.GetData();
-                foreach(var emp in  employees)
-                {
-                    Console.WriteLine($"id: {emp.id}, name: {emp.firstName}, second name: {emp.secondName}");
-                }
-            }
-            catch (Exception ex) 
-            {
-                throw new Exception(ex.Message);
-            }
-        }
-        */
     }
 }
